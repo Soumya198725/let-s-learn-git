1fdfc814a569c093b8e6a21c867129402e468e94
Soumya198725 <soumya198725@gmail.com>
2023-03-27 20:13:28 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,10 @@ variable "aws_region" {
 type = string
 default = "us-east-2"
 }
+
+variable "create_vpc" {
+  description = "Controls if VPC should be created (it affects almost all resources)"
+  type        = bool
+  default     = true
+}
+
